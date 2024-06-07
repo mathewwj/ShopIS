37da0726afe632e4b89c33c7bdfcc13686bd625b
Matúš Jakuboc <xjakuboc@fi.muni.cz>
2024-06-07 17:01:59 +0200

    map shoppinglist by id

--- api/Controllers/MapController.cs
+++ api/Controllers/MapController.cs
@@ -1,6 +1,4 @@
-﻿using api.Dto.ShoppingList;
-using api.Mappers;
-using api.Services;
+﻿using api.Services;
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 
@@ -11,17 +9,33 @@ namespace api.Controllers;
 public class MapController : ControllerBase
 {
     private readonly IPathService _pathService;
+    private readonly IShoppingListService _shoppingListService;
+    private readonly IAuthService _authService;
 
-    public MapController(IPathService pathService)
+    public MapController(IPathService pathService, IShoppingListService shoppingListService, IAuthService authService)
     {
         _pathService = pathService;
+        _shoppingListService = shoppingListService;
+        _authService = authService;
     }
 
-    [HttpPost]
+    [HttpGet]
     [Authorize]
-    public async Task<ActionResult> GetMap([FromBody] ShoppingListDto shoppingListDto)
+    [Route("{id:int}")]
+    public async Task<ActionResult> GetMap([FromRoute] int id)
     {
-        var content = await _pathService.GetSvgPath(shoppingListDto.ToShoppingListFromDto());
+        var user = await _authService.GetAppUser(User);
+        if (user == null)
+        {
+            return BadRequest("user not found");
+        }
+        var shoppingList = await _shoppingListService.GetByIdAsync(user, id);
+        if (shoppingList == null)
+        {
+            return NotFound();
+        }
+        
+        var content = await _pathService.GetSvgPath(shoppingList);
         return Content(content, "image/svg+xml");
     }
 }
